61d789f0eb849516105bad64cc3667f3c80484cf
Karol Miklasz <karol3wpl@gmail.com>
2024-01-02 22:25:32 +0100

    Add extra functions

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,19 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, session, flash, redirect, url_for
+from blog.forms import LoginForm
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm
+import functools
+
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+
+    return check_permissions
 
 
 @app.route("/")
@@ -12,6 +24,7 @@ def index():
 
 @app.route("/post/<int:entry_id>/", methods=["GET", "POST"])
 @app.route("/post/", methods=["GET", "POST"], defaults={"entry_id": None})
+@login_required
 def post(entry_id):
     if entry_id:
         entry = Entry.query.get_or_404(entry_id)
@@ -27,3 +40,42 @@ def post(entry_id):
         return redirect(url_for('index'))
 
     return render_template("entry_form.html", form=form, entry_id=entry_id)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
+
+@app.route("/drafts/", methods=['GET'])
+@login_required
+def list_drafts():
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   return render_template("drafts.html", drafts=drafts)
+
+
+@app.route("/delete-post/<int:entry_id>", methods=['POST'])
+@login_required
+def delete_entry(entry_id):
+    post = Entry.query.get_or_404(entry_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post has been deleted.', 'success')
+    return redirect(url_for('index'))
--- config.py
+++ config.py
@@ -4,4 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'bardzo_tajny_klucz'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///moja_baza_danych.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
